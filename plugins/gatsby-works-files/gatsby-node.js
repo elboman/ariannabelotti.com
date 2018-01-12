e161f2fd9ec9e2209a16fa06078774af6acb07b0
elboman <marco.botto@gmail.com>
2018-01-12 14:22:46 +0000

    feat(gatsby): add plugin for copying static files from works

---
+++ plugins/gatsby-works-files/gatsby-node.js
@@ -0,0 +1,66 @@
+const isRelativeUrl = require('is-relative-url');
+const fsExtra = require('fs-extra');
+const path = require('path');
+
+const DEPLOY_DIR = 'public';
+
+const newFileName = linkNode =>
+  `${linkNode.name}-${linkNode.internal.contentDigest}.${linkNode.extension}`;
+
+const newFilePath = (linkNode, destinationDir) => {
+  if (destinationDir) {
+    return path.posix.join(
+      process.cwd(),
+      DEPLOY_DIR,
+      destinationDir,
+      newFileName(linkNode)
+    );
+  }
+  return path.posix.join(process.cwd(), DEPLOY_DIR, newFileName(linkNode));
+};
+
+const newLinkURL = (linkNode, destinationDir) => {
+  if (destinationDir) {
+    return path.posix.join(`/`, destinationDir, newFileName(linkNode));
+  }
+  return path.posix.join(`/`, newFileName(linkNode));
+};
+
+const getAbsolutePath = (parentNode, file) => {
+  return path.posix.join(parentNode.dir, file);
+};
+
+exports.onCreateNode = ({ node, boundActionCreators }, pluginOptions = {}) => {
+  const { createNodeField } = boundActionCreators;
+  const { destinationDir, allowedExtensions = [] } = pluginOptions;
+
+  const filesToCopy = new Map();
+  if (
+    node.sourceInstanceName === 'works' &&
+    node.internal.type === 'File' &&
+    allowedExtensions.includes(node.extension)
+  ) {
+    const linkURL = newLinkURL(node, destinationDir);
+    const filePath = newFilePath(node, destinationDir);
+    filesToCopy.set(node.absolutePath, filePath);
+    createNodeField({
+      node,
+      name: 'publicUrl',
+      value: linkURL,
+    });
+  }
+
+  return Promise.all(
+    Array.from(filesToCopy, async ([filePathSource, filePathDest]) => {
+      // Don't copy anything is the file already exists at the location.
+      if (!fsExtra.existsSync(filePathDest)) {
+        try {
+          await fsExtra.ensureDir(path.dirname(filePathDest));
+          await fsExtra.copy(filePathSource, filePathDest);
+        } catch (err) {
+          console.error(`error copy ing file`, err);
+        }
+      }
+    })
+  );
+};
